1ed02187afdb74b4aa702fae9b769e778c119f66
Maciej Zieliński <zielinskimaciej@yahoo.com>
2024-11-17 17:30:01 +0100

    #6 small refactoring

--- src/main/java/org/springframework/cloud/contract/verifier/converter/resolvers/builders/SccModelBuilder.java
+++ src/main/java/org/springframework/cloud/contract/verifier/converter/resolvers/builders/SccModelBuilder.java
@@ -3,10 +3,8 @@ package org.springframework.cloud.contract.verifier.converter.resolvers.builders
 import com.fasterxml.jackson.databind.JsonNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.contract.verifier.converter.YamlContract;
 import org.springframework.cloud.contract.verifier.converter.YamlContract.*;
 
-import java.util.Optional;
 import java.util.function.Function;
 
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
@@ -22,6 +20,20 @@ public class SccModelBuilder {
         throw new IllegalStateException("Utility class");
     }
 
+    public static Named toNamed(JsonNode node) {
+        Named named = new Named();
+        named.paramName = toText(node.get(PARAM_NAME));
+        named.fileName = toText(node.get(FILE_NAME));
+        named.fileContent = toText(node.get(FILE_CONTENT));
+        named.fileContentAsBytes = toText(node.get(FILE_CONTENT_AS_BYTES));
+        named.fileContentFromFileAsBytes = toText(node.get(FILE_CONTENT_FROM_FILE_AS_BYTES));
+        named.contentType = toText(node.get(CONTENT_TYPE));
+        named.fileNameCommand = toText(node.get(FILE_NAME_COMMAND));
+        named.fileContentCommand = toText(node.get(FILE_CONTENT_COMMAND));
+        named.contentTypeCommand = toText(node.get(CONTENT_TYPE_COMMAND));
+        return named;
+    }
+
     public static BodyStubMatcher toBodyStubMatcher(JsonNode node) {
         BodyStubMatcher matcher = new BodyStubMatcher();
         matcher.path = toText(node.get(PATH));
@@ -30,75 +42,78 @@ public class SccModelBuilder {
         matcher.minOccurrence = toInteger(node.get(MIN_OCCURRENCE));
         matcher.maxOccurrence = toInteger(node.get(MAX_OCCURRENCE));
         matcher.regexType = createRegexType(toText(node.get(REGEX_TYPE)));
-        matcher.type = Optional.ofNullable(node.get(TYPE))
-                .map(JsonNode::asText)
-                .map(YamlContract.StubMatcherType::valueOf)
-                .orElse(null);
+        matcher.type = createStubMatcherType(toText(node.get(TYPE)));
         return matcher;
     }
 
     public static BodyTestMatcher toBodyTestMatcher(JsonNode node) {
-        BodyTestMatcher bodyStubMatcher = new BodyTestMatcher();
-        bodyStubMatcher.path = toText(node.get(PATH));
-        bodyStubMatcher.value = toText(node.get(VALUE));
-        bodyStubMatcher.predefined = createPredefinedRegex(toText(node.get(PREDEFINED)));
-        bodyStubMatcher.minOccurrence = toInteger(node.get(MIN_OCCURRENCE));
-        bodyStubMatcher.maxOccurrence = toInteger(node.get(MAX_OCCURRENCE));
-        bodyStubMatcher.regexType = createRegexType(toText(node.get(REGEX_TYPE)));
-        if (node.get(TYPE) != null) {
-            bodyStubMatcher.type = YamlContract.TestMatcherType.valueOf(toText(node.get(TYPE)));
-        }
-        return bodyStubMatcher;
+        BodyTestMatcher matcher = new BodyTestMatcher();
+        matcher.path = toText(node.get(PATH));
+        matcher.value = toText(node.get(VALUE));
+        matcher.predefined = createPredefinedRegex(toText(node.get(PREDEFINED)));
+        matcher.minOccurrence = toInteger(node.get(MIN_OCCURRENCE));
+        matcher.maxOccurrence = toInteger(node.get(MAX_OCCURRENCE));
+        matcher.regexType = createRegexType(toText(node.get(REGEX_TYPE)));
+        matcher.type = createTestMatcherType(toText(node.get(TYPE)));
+        return matcher;
     }
 
     public static TestHeaderMatcher toTestHeaderMatcher(JsonNode node) {
-        TestHeaderMatcher headersMatcher = new TestHeaderMatcher();
-        headersMatcher.key = toText(node.get(KEY));
-        headersMatcher.regex = toText(node.get(REGEX));
-        headersMatcher.predefined = createPredefinedRegex(toText(node.get(PREDEFINED)));
-        headersMatcher.command = toText(node.get(COMMAND));
-        headersMatcher.regexType = createRegexType(toText(node.get(REGEX_TYPE)));
-        return headersMatcher;
+        TestHeaderMatcher matcher = new TestHeaderMatcher();
+        matcher.key = toText(node.get(KEY));
+        matcher.regex = toText(node.get(REGEX));
+        matcher.predefined = createPredefinedRegex(toText(node.get(PREDEFINED)));
+        matcher.command = toText(node.get(COMMAND));
+        matcher.regexType = createRegexType(toText(node.get(REGEX_TYPE)));
+        return matcher;
     }
 
     public static TestCookieMatcher toTestCookieMatcher(JsonNode node) {
-        TestCookieMatcher testCookieMatcher = new TestCookieMatcher();
-        testCookieMatcher.key = toText(node.get(KEY));
-        testCookieMatcher.regex = toText(node.get(REGEX));
-        testCookieMatcher.predefined = createPredefinedRegex(toText(node.get(PREDEFINED)));
-        testCookieMatcher.command = toText(node.get(COMMAND));
-        testCookieMatcher.regexType = createRegexType(toText(node.get(REGEX_TYPE)));
-        return testCookieMatcher;
+        TestCookieMatcher matcher = new TestCookieMatcher();
+        matcher.key = toText(node.get(KEY));
+        matcher.regex = toText(node.get(REGEX));
+        matcher.predefined = createPredefinedRegex(toText(node.get(PREDEFINED)));
+        matcher.command = toText(node.get(COMMAND));
+        matcher.regexType = createRegexType(toText(node.get(REGEX_TYPE)));
+        return matcher;
     }
 
-    public static QueryParameterMatcher toQueryParameterMatcher(String parameterName, JsonNode matcher) {
-        QueryParameterMatcher queryParameterMatcher = new QueryParameterMatcher();
-        queryParameterMatcher.key = parameterName;
-        queryParameterMatcher.value = matcher.get(VALUE);
-        queryParameterMatcher.type = createMatchingType(matcher.get(TYPE).asText());
-        return queryParameterMatcher;
+    public static QueryParameterMatcher toQueryParameterMatcher(String parameterName, JsonNode node) {
+        QueryParameterMatcher matcher = new QueryParameterMatcher();
+        matcher.key = parameterName;
+        matcher.value = node.get(VALUE);
+        matcher.type = createMatchingType(node.get(TYPE).asText());
+        return matcher;
     }
 
-    public static  KeyValueMatcher toKeyValueMatcher(String name, JsonNode matcher) {
-        KeyValueMatcher keyValueMatcher = new KeyValueMatcher();
-        keyValueMatcher.key = name;
-        keyValueMatcher.regex = toText(matcher.get(REGEX));
-        keyValueMatcher.predefined = createPredefinedRegex(toText(matcher.get(PREDEFINED)));
-        keyValueMatcher.command = toText(matcher.get(COMMAND));
-        keyValueMatcher.regexType = createRegexType(toText(matcher.get(REGEX_TYPE)));
-        return keyValueMatcher;
+    public static KeyValueMatcher toKeyValueMatcher(String name, JsonNode node) {
+        KeyValueMatcher matcher = new KeyValueMatcher();
+        matcher.key = name;
+        matcher.regex = toText(node.get(REGEX));
+        matcher.predefined = createPredefinedRegex(toText(node.get(PREDEFINED)));
+        matcher.command = toText(node.get(COMMAND));
+        matcher.regexType = createRegexType(toText(node.get(REGEX_TYPE)));
+        return matcher;
     }
 
-    private static YamlContract.MatchingType createMatchingType(String val) {
-        return tryToParse(val, YamlContract.MatchingType::valueOf);
+    private static TestMatcherType createTestMatcherType(String val) {
+        return tryToParse(val, TestMatcherType::valueOf);
     }
 
-    private static YamlContract.PredefinedRegex createPredefinedRegex(String val) {
-        return tryToParse(val, YamlContract.PredefinedRegex::valueOf);
+    private static StubMatcherType createStubMatcherType(String val) {
+        return tryToParse(val, StubMatcherType::valueOf);
     }
 
-    private static YamlContract.RegexType createRegexType(String val) {
-        return tryToParse(val, YamlContract.RegexType::valueOf);
+    private static MatchingType createMatchingType(String val) {
+        return tryToParse(val, MatchingType::valueOf);
+    }
+
+    private static PredefinedRegex createPredefinedRegex(String val) {
+        return tryToParse(val, PredefinedRegex::valueOf);
+    }
+
+    private static RegexType createRegexType(String val) {
+        return tryToParse(val, RegexType::valueOf);
     }
 
     private static <T> T tryToParse(String val, Function<String, T> function) {
--- src/main/java/org/springframework/cloud/contract/verifier/converter/resolvers/request/multipart/RequestMultipartResolver.java
+++ src/main/java/org/springframework/cloud/contract/verifier/converter/resolvers/request/multipart/RequestMultipartResolver.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.cloud.contract.verifier.converter.Oa3Spec;
 import org.springframework.cloud.contract.verifier.converter.YamlContract;
 import org.springframework.cloud.contract.verifier.converter.YamlContract.Multipart;
+import org.springframework.cloud.contract.verifier.converter.resolvers.builders.SccModelBuilder;
 import org.springframework.cloud.contract.verifier.converter.resolvers.request.AbstractResolver;
 
 import java.util.List;
@@ -12,7 +13,6 @@ import java.util.stream.Collectors;
 
 import static org.springframework.cloud.contract.verifier.converter.Oa3Spec.*;
 import static org.springframework.cloud.contract.verifier.converter.Utils.toStream;
-import static org.springframework.cloud.contract.verifier.converter.Utils.toText;
 
 public class RequestMultipartResolver extends AbstractResolver<Multipart> {
 
@@ -34,19 +34,8 @@ public class RequestMultipartResolver extends AbstractResolver<Multipart> {
 
     private static List<YamlContract.Named> namedToList(Map<String, JsonNode> multipart) {
         return toStream(multipart.get(NAMED).iterator())
-                .map(named -> {
-                    YamlContract.Named yamlNamed = new YamlContract.Named();
-                    yamlNamed.paramName = toText(named.get(PARAM_NAME));
-                    yamlNamed.fileName = toText(named.get(FILE_NAME));
-                    yamlNamed.fileContent = toText(named.get(FILE_CONTENT));
-                    yamlNamed.fileContentAsBytes = toText(named.get(FILE_CONTENT_AS_BYTES));
-                    yamlNamed.fileContentFromFileAsBytes = toText(named.get(FILE_CONTENT_FROM_FILE_AS_BYTES));
-                    yamlNamed.contentType = toText(named.get(CONTENT_TYPE));
-                    yamlNamed.fileNameCommand = toText(named.get(FILE_NAME_COMMAND));
-                    yamlNamed.fileContentCommand = toText(named.get(FILE_CONTENT_COMMAND));
-                    yamlNamed.contentTypeCommand = toText(named.get(CONTENT_TYPE_COMMAND));
-                    return yamlNamed;
-                }).toList();
+                .map(SccModelBuilder::toNamed)
+                .toList();
     }
 
     private static Map<String, String> paramsToMap(Map<String, JsonNode> multipart) {
